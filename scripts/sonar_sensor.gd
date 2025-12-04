cdf0d2204341844d77200662e88fa9f620914f21
Drew Finstrom <dgf1979@users.noreply.github.com>
2025-12-04 11:13:40 -0800

    refactor collider target detection; shade blip to team color
    -broke out node type detection into discreet functions
    -shade the sonar blip with the team color when colliding node is detected as a ship

--- scripts/sonar_sensor.gd
+++ scripts/sonar_sensor.gd
@@ -20,11 +20,28 @@ func _physics_process(_delta):
 		var collider = get_collider()
 		#prevent re-firing on each tick while colliding remains true
 		if collider.get_instance_id() != last_collider_id:
-			last_collider_id = collider.get_instance_id() 
-			var coll_pos = get_collision_point() - get_global_position()
-			var coll_color = Color.AZURE
-			if collider.has_node("team"):
-				coll_color = collider.modulate
-			$"../GUI/sonar_display".add_display_object(coll_pos, coll_color)
+			last_collider_id = collider.get_instance_id()
+			_handle_collision(collider, get_collision_point()) 
 	else:
 		last_collider_id = 0
+		
+func _handle_collision(collider, collision_point):
+	if _is_ship(collider):
+		_fire_sonar_ship_blip(collider, collision_point)
+	else:
+		_fire_sonar_generic_blip(collision_point)
+	
+func _is_ship(collider) -> bool:
+	return collider.has_node("team")
+	
+func _fire_sonar_ship_blip(collider: BattleCharacter, collision_point):
+	var coll_pos = collision_point - get_global_position()
+	var coll_color = collider.get_node("team").color
+	$"../GUI/sonar_display".add_display_object(coll_pos, coll_color)
+	
+func _fire_sonar_generic_blip(collision_point):
+	var coll_pos = collision_point - get_global_position()
+	var coll_color = Color.CHARTREUSE
+	$"../GUI/sonar_display".add_display_object(coll_pos, coll_color)
+	
+		
