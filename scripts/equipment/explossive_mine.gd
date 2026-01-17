11e818b7230ebd71f6dc56ee2180bb9f224df8c8
lostpoet999 <williamsjasonm@hotmail.com>
2026-01-17 12:49:34 -0600

    mine - added quick pulsing tween

--- scripts/equipment/explossive_mine.gd
+++ scripts/equipment/explossive_mine.gd
@@ -1,21 +1,26 @@
 extends Area2D
 
 var is_activated = false
-@onready var level = get_tree().current_scene
+@onready var level = get_tree().current_scene	
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("drop_mine") and not is_activated:
 		deploy_mine()
 
-func deploy_mine() -> void:
-	print("tree: ", get_tree())
-	print("current scene: ", get_tree().current_scene)
-	print("root Children: ", get_tree().get_root().get_children())
-	print("owner: ", owner)
+func run_deployed_tween():
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1.3, 1.3), .5)
+	tween.tween_property(self, "scale", Vector2(1, 1), .5)
+	tween.set_loops(0)
+	#tween.play()
+
+func deploy_mine() -> void:	
 	var drop_position = get_global_position()
 	var drop_rotation = get_global_rotation()
 
 	get_parent().remove_child(self)
 	level.add_child(self)
 	set_global_position(drop_position)
-	set_global_rotation(drop_rotation)
+	set_global_rotation(0)
+	run_deployed_tween()
+	is_activated = true
