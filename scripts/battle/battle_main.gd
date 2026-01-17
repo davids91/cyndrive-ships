0b08322f0227ee453aad99a631247e97816e39e5
davids91 <toth.david.munka@gmail.com>
2026-01-17 12:10:13 +0100

    feat: Star Nebula display

--- scripts/battle/battle_main.gd
+++ scripts/battle/battle_main.gd
@@ -134,9 +134,9 @@ var reverse_hold_time_sec: float = 0.
 var reverse_tap_count: int = 0
 var reverse_last_tap_at: int = Time.get_ticks_msec()
 var replay_viewport = Rect2()
-@onready var battle_start_timetamp_msec: int = int(Time.get_unix_time_from_system())
+@onready var battle_start_timestamp_msec: int = int(Time.get_unix_time_from_system())
 func _process(delta):
-	var display_time: int = battle_start_timetamp_msec + int(BattleTimeline.instance.time_msec())
+	var display_time: int = battle_start_timestamp_msec + int(BattleTimeline.instance.time_msec())
 	$GUI/debug_stats/fps.set_text("%s fps" % Engine.get_frames_per_second())
 	$GUI/time.set_text("0x%X//%X" % [display_time >> 16, display_time & 0xFFFF])
 	$GUI/sensors_display.get_material().set_shader_parameter("aspect_ratio", get_viewport().size.x/ get_viewport().size.y)
