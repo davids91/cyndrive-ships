ae4bcb9470d61863be06a89b2b14c908e474b0ab
davids91 <toth.david.munka@gmail.com>
2026-01-29 01:55:47 +0100

    feat: black hole death trap

---
+++ scripts/effects/black_hole.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+
+var bodies_within: Dictionary
+func _on_body_entered(body: Node2D) -> void:
+	bodies_within[body] = BattleTimeline.instance.time_msec()
+
+func _on_body_exited(body: Node2D) -> void:
+	bodies_within.erase(body)
+@export var strength: float = 5.
+@export var time_to_die_msec: float = 2000.
+@export var death_radius: float = 250
+func _process(_delta: float) -> void:
+	for body in bodies_within:
+		var body_to_center = (get_global_position() - body.get_global_position())
+		var pull_force = body_to_center.normalized()
+		pull_force *= strength * (BattleTimeline.instance.time_msec() - bodies_within[body]) / time_to_die_msec
+		body.apply_impulse(pull_force)
+		if body_to_center.length() < death_radius:
+			if body.has_method("accept_damage"): body.accept_damage(666)
+			else:
+				bodies_within.erase(body)
+				body.queue_free()
