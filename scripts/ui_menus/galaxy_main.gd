168d270a6503f3ad8cdb57b708da8c6c55d80c6c
davids91 <toth.david.munka@gmail.com>
2026-02-21 03:09:54 +0100

    feat: display guide graphics from menu

--- scripts/ui_menus/galaxy_main.gd
+++ scripts/ui_menus/galaxy_main.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-@export var star_speed: float = 0.05
+@export var star_speed: float = 0.005
 
+var speed = star_speed
 var angle: float = 0.
 func _process(delta_time: float) -> void:
-	angle += star_speed * delta_time
+	angle += speed * delta_time
 
 	# Update the stars
 	$StarParticles.get_process_material().set_shader_parameter("angle", angle);
@@ -21,3 +22,18 @@ func _on_tutorial_level_button_scene_selected() -> void:
 
 func _on_mr_mustle_battle_pressed() -> void:
 	get_tree().change_scene_to_packed(preload("res://scenes/battles/mr_mustle.tscn"))
+
+func _on_guides_button_pressed() -> void:
+	speed = 0.
+	$guides_cam.make_current()
+	$guides_cam.offset = $movement_guide.position
+
+func _on_back_button_pressed() -> void:
+	speed = star_speed
+	$main_cam.make_current()
+
+func _on_next_button_pressed() -> void:
+	$guides_cam.offset = $weapons_guide.position - Vector2(150., 0.)
+
+func _on_next_button_pressed_2() -> void:
+	$guides_cam.offset = $time_travel_guide.position
